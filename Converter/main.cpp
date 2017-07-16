6e72c9a0b523287049e624da654f4dc591313801
mmlmml1 <huqingyang2000@gmail.com>
2017-07-15 21:17:13 -0400

    Initialize the project

---
+++ Converter/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+using namespace std;
+struct note{
+    int time;
+    int frequency;
+    int length;
+};
+int returnKeys(int frequency,vector<int> frequencies);
+int main() {
+    freopen("/Users/hqy/CLionProjects/coverter/happy.in","r",stdin);
+    freopen("/Users/hqy/CLionProjects/coverter/array.csv","w",stdout);
+    int time,notes,length;
+    int maximum = 0;
+    int count;
+    vector<note> scores;
+    vector<int> frequencies;
+    while(cin>>time>>notes>>length){
+        length ++;
+        length = length / 50;
+        time = time / 50;
+        note a = {
+                .frequency = notes,
+                .time = time,
+                .length = length,
+        };
+        scores.push_back(a);
+        if(length+time>maximum){
+            maximum = length+time;
+        }
+        bool exist = false;
+        for(int i=0; i< frequencies.size(); i++){
+            if(notes == frequencies[i]){
+                exist = true;
+            }
+        }
+        if(!exist){
+            frequencies.push_back(notes);
+        }
+    }
+    sort(frequencies.begin(), frequencies.end());
+    bool matrix[maximum][frequencies.size()];
+    memset(matrix,false,sizeof(matrix));
+    for(int i=0;i<scores.size();i++){
+        int key = returnKeys(scores[i].frequency,frequencies);
+        //cout<<key<<endl;
+        for(int j=scores[i].time;j<scores[i].time+scores[i].length-1;j++){
+            matrix[j][key] = true;
+        }
+    }
+    //cout<<"keys"<<frequencies.size()-
+    cout<<"//BEGIN AUTO-GENERATED ZONE"<<endl;
+    cout<<"const int keys = "<<frequencies.size()<<";"<<endl;
+    cout<<"const int length = "<<maximum<<";"<<endl;
+    cout<<"const bool notes["<<maximum<<"]["<<frequencies.size()<<"] = {";
+    for(int i=0;i<maximum;i++){
+        for(int j=0;j<=frequencies.size()-1;j++){
+            if(matrix[i][j] == true)
+                cout<<"true"<<", ";
+            else
+                cout<<"false"<<", ";
+        }
+        //cout<<endl;
+    }
+    cout<<"};"<<endl;
+    cout<<"//END AUTO-GENERATED ZONE"<<endl;
+    //cout<<maximum;
+    return 0;
+}
+int returnKeys(int frequency,vector<int> frequencies){
+    for(int i=0;i<=frequencies.size();i++){
+        if(frequencies[i] == frequency)
+            return i;
+    }
+}
---
+++ ESP/ESP.ino
@@ -0,0 +1,96 @@
+
+//BEGIN AUTO-GENERATED ZONE
+const int keys = 5;
+const int length = 260;
+const bool notes[260][5] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, };
+//END AUTO-GENERATED ZONE
+
+#define boardtime unsigned long
+
+#include <algorithm>
+using namespace std;
+class SolenoidController {
+  private:
+    const int diffTime = 40;
+    const int interval = 50;
+    boardtime startTime = 0;
+    boardtime lastUpdatedTime = 0;
+    bool isPlaying = false;
+    virtual bool* getNotes(boardtime currentTime){
+      int currentRound = (int)((currentTime - this->startTime) / this->interval);
+      int timeInterval = (currentTime - this->startTime) - currentRound * 50; 
+      bool currentNote[keys];
+      copy(begin(notes[currentRound]),end(notes[currentRound]),begin(currentNote));
+      if(timeInterval <= diffTime && currentRound != 0){
+        bool lastNote[keys];
+        bool finalNote[keys];
+        copy(begin(notes[currentRound - 1]),end(notes[currentRound - 1]),begin(lastNote));
+        
+        for(int i=0;i<keys;i++){
+          if(lastNote[i] == true && currentNote[i] == false){
+            finalNote[i] = true;
+          } else {
+            finalNote[i] = currentNote[i];
+          }
+        }
+        return finalNote;
+      } else {
+        return currentNote;
+      }
+    }
+  public:
+    virtual void startPlaying(){
+      this->startTime = millis();
+      this->isPlaying = true;
+      Serial.println("Start playing!");
+    }
+    virtual void pausePlaying(){
+      this->lastUpdatedTime = 0;
+      this->isPlaying = false;
+    }
+    virtual void resumePlaying(){
+      this->isPlaying = true;
+    }
+    virtual void stopPlaying(){
+      this->isPlaying = false;
+      this->lastUpdatedTime = 0;
+    }
+    virtual void periodUpdate(){
+      boardtime currentTime = millis();
+      if(this->isPlaying){ //playing
+        bool* note = this->getNotes(currentTime);
+        this->lastUpdatedTime = currentTime;
+        for(int i = 0; i<keys; i++)
+          Serial.print(note[i]);
+        Serial.println(" ");
+      } else {
+        if(this->lastUpdatedTime != 0){ //pausing
+          this->startTime += currentTime - this->lastUpdatedTime;
+          this->lastUpdatedTime = currentTime;
+        } else { //stoping
+        }
+      }
+      if((currentTime - this->startTime) > length * 50){
+        this->stopPlaying();
+        this->startPlaying();
+        Serial.println("Restart!");
+      }
+    }
+};
+
+SolenoidController *mySolenoid;
+
+void setup() {
+  mySolenoid = new SolenoidController();
+  ESP.wdtDisable();
+  Serial.begin(115200);
+  mySolenoid->startPlaying();
+}
+
+void loop() {
+  mySolenoid->periodUpdate();
+  ESP.wdtFeed();
+  delay(10);
+}
+
+
