6eb5835575493178b10b09c6eb96188ebbc91734
mmlmml1 <huqingyang2000@gmail.com>
2017-07-16 20:35:59 -0400

    Update notes list

--- Converter/main.cpp
+++ Converter/main.cpp
@@ -17,20 +17,23 @@
 #include <cstdlib>
 #include <cstdio>
 #include <vector>
+#include <string>
 using namespace std;
+const string midiNotes[128] = {"C0","C#0","D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0","C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1","C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2","C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3","C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5","C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6","C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7","C8","C#8","D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8","C9","C#9","D9","D#9","E9","F9","F#9","G9","G#9","A9","A#9","B9","C10","C#10","D10","D#10","E10","F10","F#10","G10"};
 struct note{
     int time;
     int frequency;
     int length;
 };
-const int interval = 40; // The interval of the midi
+const int interval = 50; // The interval of the midi
 const bool addOne = false; // Add one to check stop (set to true when the duration has figures like 499 249 etc)
 const bool removeBlank = true; // Remove the blank part at the front
 const bool displayMatrix = false; // Display the matrix of the notes
+const bool outputKeysNeeded = true; // Output the keys it needed to play the whole song
 int returnKeys(int frequency,vector<int> frequencies);
 int main() {
     freopen("/Users/hqy/Documents/Github/ESAP17-Robotics-Keyboard/Converter/examples/your_name.in","r",stdin);
-    freopen("/Users/hqy/Documents/Github/ESAP17-Robotics-Keyboard/Converter/examples/your_name.out","w",stdout);
+    //freopen("/Users/hqy/Documents/Github/ESAP17-Robotics-Keyboard/Converter/examples/your_name.out","w",stdout);
     int time,notes,length;
     int maximum = 0;
     int blank = 1048576;
@@ -79,10 +82,18 @@ int main() {
             matrix[j][key] = true;
         }
     }
+    if(outputKeysNeeded){
+        cout<<"Keys needed:";
+        for(int i=0;i<frequencies.size();i++)
+            if(frequencies[i]<128 && frequencies[i]>0)
+                cout<<midiNotes[frequencies[i]]<<" ";
+            else
+                cout<<frequencies[i]<<" ";
+        cout<<endl;
+    }
     if(displayMatrix){
-
         for(int i=0;i<maximum;i++){
-            for(int j=0;j<=frequencies.size()-1;j++){
+            for(int j=0;j<frequencies.size();j++){
                 if(matrix[i][j] == true)
                     cout<<"1";
                 else
